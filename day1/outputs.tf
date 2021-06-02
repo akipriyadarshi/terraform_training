bc06f914c12fbe0271c38222d1ffb2bba43bb3fe
akash <akash.priyadarshi@knoldus.com>
2021-06-02 09:55:18 +0530

    added handson

---
+++ day1/outputs.tf
@@ -0,0 +1,4 @@
+output "instance_ip" {
+    value = aws_instance.instance1.public_ip
+  
+}
---
+++ day1/variables.tf
@@ -0,0 +1,4 @@
+variable "my_instance_type" {
+  type = string
+  description = "my own instance type"
+}
